7253e9274ae54f914c195b8d63f499fd3f4ec70d
Oskar Månsson <o92design@gmail.com>
2021-07-23 17:55:00 +0200

    Updated for 2021 and URP

--- InputSystem_Warriors_Project/Assets/Scripts/Behaviours/Player/PlayerMovementBehaviour.cs
+++ InputSystem_Warriors_Project/Assets/Scripts/Behaviours/Player/PlayerMovementBehaviour.cs
@@ -4,72 +4,71 @@ using UnityEngine;
 
 public class PlayerMovementBehaviour : MonoBehaviour
 {
+  [Header("Component References")]
+  public Rigidbody playerRigidbody;
 
-    [Header("Component References")]
-    public Rigidbody playerRigidbody;
-
-    [Header("Movement Settings")]
-    public float movementSpeed = 3f;
-    public float turnSpeed = 0.1f;
-
-
-    //Stored Values
-    private Camera mainCamera;
-    private Vector3 movementDirection;
+  [Header("Movement Settings")]
+  public float movementSpeed = 3f;
+  public float turnSpeed = 0.1f;
 
 
-    public void SetupBehaviour()
+  //Stored Values
+  private Camera mainCamera;
+  private Vector3 movementDirection;
+
+
+  public void SetupBehaviour()
+  {
+    SetGameplayCamera();
+  }
+
+  void SetGameplayCamera()
+  {
+    mainCamera = CameraManager.Instance.GetGameplayCamera();
+  }
+
+  public void UpdateMovementData(Vector3 newMovementDirection)
+  {
+    movementDirection = newMovementDirection;
+  }
+
+  void FixedUpdate()
+  {
+    MoveThePlayer();
+    TurnThePlayer();
+  }
+
+  void MoveThePlayer()
+  {
+    Vector3 movement = CameraDirection(movementDirection) * movementSpeed * Time.deltaTime;
+    playerRigidbody.MovePosition(transform.position + movement);
+  }
+
+  void TurnThePlayer()
+  {
+    if (movementDirection.sqrMagnitude > 0.01f)
     {
-        SetGameplayCamera();
+
+      Quaternion rotation = Quaternion.Slerp(playerRigidbody.rotation,
+                                           Quaternion.LookRotation(CameraDirection(movementDirection)),
+                                           turnSpeed);
+
+      playerRigidbody.MoveRotation(rotation);
+
     }
-
-    void SetGameplayCamera()
-    {
-        mainCamera = CameraManager.Instance.GetGameplayCamera();
-    }
-
-    public void UpdateMovementData(Vector3 newMovementDirection)
-    {
-        movementDirection = newMovementDirection;
-    }
-
-    void FixedUpdate()
-    {
-        MoveThePlayer();
-        TurnThePlayer();
-    }
-
-    void MoveThePlayer()
-    {
-        Vector3 movement = CameraDirection(movementDirection) * movementSpeed * Time.deltaTime;
-        playerRigidbody.MovePosition(transform.position + movement);
-    }
-    
-    void TurnThePlayer()
-    {
-        if(movementDirection.sqrMagnitude > 0.01f)
-        {
-
-             Quaternion rotation = Quaternion.Slerp(playerRigidbody.rotation,
-                                                  Quaternion.LookRotation (CameraDirection(movementDirection)),
-                                                  turnSpeed);
-
-            playerRigidbody.MoveRotation(rotation);
-
-        }
-    }
+  }
 
 
-    Vector3 CameraDirection(Vector3 movementDirection)
-    {
-        var cameraForward = mainCamera.transform.forward;
-        var cameraRight = mainCamera.transform.right;
+  Vector3 CameraDirection(Vector3 movementDirection)
+  {
+    var cameraForward = mainCamera.transform.forward;
+    var cameraRight = mainCamera.transform.right;
 
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
-        
-        return cameraForward * movementDirection.z + cameraRight * movementDirection.x; 
-   
-    }
+    cameraForward.y = 0f;
+    cameraRight.y = 0f;
+
+    return cameraForward * movementDirection.z + cameraRight * movementDirection.x;
+
+  }
 
 }
